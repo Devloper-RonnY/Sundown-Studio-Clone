153c7bc7e78b23364903b5a943f77978303106b5
Devloper-RonnY <roshanpawar48640@gmail.com>
2024-05-30 13:27:18 +0530

    adding footer section

--- script.js
+++ script.js
@@ -3,26 +3,38 @@ const scroll = new LocomotiveScroll({
     smooth: true
 });
 
- var elemC = document.querySelector(`#elem-container`);
- var image = document.querySelector(`#fixed-image`)
-
- elemC.addEventListener(`mouseenter`, ()=>{
-    image.style.display = `block`
- })
- elemC.addEventListener(`mouseleave`, ()=>{
-    image.style.display = `none`
- })
-
-//  var elements = document.querySelector(`#elem1`)
-//  elements.addEventListener(`mouseenter`, ()=>{
-//     let image1 = elements.getAttribute(`data-image`)
-//     image.style.backgroundImage = `url(${image1})`
-//  })
-
-let elements = document.querySelectorAll(`#elem1`)
-elements.forEach( (e)=>{
-    e.addEventListener("mouseenter", ()=>{
-        let img = e.getAttribute("data-image")
-        image.style.backgroundImage = `url(${img})`
+function page4Animation (){
+    var elemC = document.querySelector(`#elem-container`);
+    var image = document.querySelector(`#fixed-image`)
+   
+    elemC.addEventListener(`mouseenter`, ()=>{
+       image.style.display = `block`
     })
-})
+    elemC.addEventListener(`mouseleave`, ()=>{
+       image.style.display = `none`
+    })
+   
+   
+   let elements = document.querySelectorAll(`#elem1`)
+   elements.forEach( (e)=>{
+       e.addEventListener("mouseenter", ()=>{
+           let img = e.getAttribute("data-image")
+           image.style.backgroundImage = `url(${img})`
+       })
+   })
+}
+
+function swipperAnimation (){
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: "auto",
+        centeredSlides: false,
+        // screenLeft: false,
+        spaceBetween: 100,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
+}
+swipperAnimation();
+page4Animation();
