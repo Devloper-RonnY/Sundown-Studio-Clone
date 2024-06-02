ff2dfd5128b7b58ceb156ecba8e67147dad730a7
Devloper-RonnY <roshanpawar48640@gmail.com>
2024-06-02 08:31:19 +0530

    add mid responsiveness

--- script.js
+++ script.js
@@ -23,18 +23,30 @@ function page4Animation (){
        })
    })
 }
-
 function swipperAnimation (){
     var swiper = new Swiper(".mySwiper", {
         slidesPerView: "auto",
         centeredSlides: false,
         // screenLeft: false,
         spaceBetween: 100,
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
       });
 }
 swipperAnimation();
 page4Animation();
+
+var navimg = document.querySelector(`nav img`)
+var full = document.querySelector(`#full-scr`)
+var menu = document.querySelector(`nav h3`);
+var flag = 0
+menu.addEventListener(`click`, function(){
+    if(flag == 0){
+        full.style.top = 0
+        navimg.style.opacity = 0
+        flag = 1
+    } else {
+        full.style.top = "-100%"
+        navimg.style.opacity = 1
+        flag = 0
+    }
+    
+})
